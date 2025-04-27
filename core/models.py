87e16d6280bc4c165b54a34e93ffe04dcc2ea20f
SomayaElgendy <somaya.hussein2004@gmail.com>
2025-04-27 21:40:30 +0300

    Added Driver role to Profile model

--- core/models.py
+++ core/models.py
@@ -7,6 +7,7 @@ class Profile(AbstractUser):
         ('admin', 'Admin'),
         ('staff', 'Staff'),
         ('passenger', 'Passenger'),
+        ('DRIVER', 'Driver'),
     ]
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
